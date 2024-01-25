755fc1a2c48c97f33198f864ddd2ac55e1cec412
kreison <alankainazovr@gmail.com>
2024-01-25 14:26:18 +0600

    sfdsa

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config'
 import ffmpeg from 'fluent-ffmpeg';
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg');
-ffmpeg.setFfmpegPath(ffmpegPath.path);
-console.log(ffmpegPath.path,'ffmpegPath.path)');
 if (process.env.NODE_ENV !== 'production') {
+    const ffmpegPath = require('@ffmpeg-installer/ffmpeg');
+    ffmpeg.setFfmpegPath(ffmpegPath.path);
+    console.log(ffmpegPath.path,'ffmpegPath.path)');
+}else {
+    ffmpeg.setFfmpegPath('/bin/ffmpeg/');
+
 }
 
 
